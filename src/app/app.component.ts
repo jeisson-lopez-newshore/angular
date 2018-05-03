4959ab4b909954bf202d92ef476a8d9204679c12
Jeisson Lopez <jalopez@todo1.com>
2018-05-03 13:28:01 -0500

    Desarrollo ejercicio Angular Branch 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  person  = 'jason lopez';
+  title = 'My application';
+  author = 'Jeisson Lopez';
 }
