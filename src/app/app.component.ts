b9d8f7e675ed9612119a6d2babbc7ab0893c772f
Jeisson Lopez <jalopez@todo1.com>
2018-05-12 17:19:42 -0500

    Desarrollo tarea 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
-
+	title = 'My application';
+	author = 'Jeisson López';
+	count = 0;
+	imageSource = './../favicon.ico'
 }
