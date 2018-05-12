6d34cf2e180abbab27bbe4481e34136aa1d11957
Jeisson Lopez <jalopez@todo1.com>
2018-05-12 15:31:27 -0500

    Se completa el desarrollo

--- src/app/comments/comments/comments.component.ts
+++ src/app/comments/comments/comments.component.ts
@@ -1,24 +1,22 @@
 import { Post } from './../../entities/post.interface';
 import { ResourceService } from './../../share/resource.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 @Component({
-  selector: 'app-comments',
-  templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.scss']
+	selector: 'app-comments',
+	templateUrl: './comments.component.html',
+	styleUrls: [ './comments.component.scss' ]
 })
 export class CommentsComponent implements OnInit {
+	url = 'http://jsonplaceholder.typicode.com/comments';
+	dataObs$: Observable<Comment[]>;
+	@Input() data: string;
 
-  url = 'http://jsonplaceholder.typicode.com/comments';
-  dataObs$: Observable<Comment[]>;
+	constructor(service: ResourceService) {
+		this.dataObs$ = service.getResource<Comment>(this.url, 'body');
+		console.log(this.dataObs$);
+	}
 
-  constructor(service: ResourceService) {
-    this.dataObs$ = service.getResource<Comment>(this.url, 'body');
-    console.log(this.dataObs$);
-  }
-
-  ngOnInit() {
-  }
-
+	ngOnInit() {}
 }
--- src/app/posts/posts/posts.component.ts
+++ src/app/posts/posts/posts.component.ts
@@ -4,20 +4,17 @@ import { Observable } from 'rxjs/Observable';
 import { Post } from '../../entities/Post.interface';
 
 @Component({
-  selector: 'app-posts',
-  templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+	selector: 'app-posts',
+	templateUrl: './posts.component.html',
+	styleUrls: [ './posts.component.scss' ]
 })
 export class PostsComponent implements OnInit {
+	url = 'http://jsonplaceholder.typicode.com/posts';
+	dataObs$: Observable<Post[]>;
 
-  url = 'http://jsonplaceholder.typicode.com/posts';
-  dataObs$: Observable<Post[]>;
+	constructor(service: ResourceService) {
+		this.dataObs$ = service.getResource<Post>(this.url, 'title');
+	}
 
-  constructor(service: ResourceService) {
-    this.dataObs$ = service.getResource<Post>(this.url, 'title');
-  }
-
-  ngOnInit() {
-  }
-
+	ngOnInit() {}
 }
